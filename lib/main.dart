8d00837d7aec3bd464795e2efb7fd56c9aee9fc1
AbhishekKr <abhikumar163@gmail.com>
2019-06-01 18:59:58 +0530

    fetching hackernews topstories using http and json decoding
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 void main() => runApp(HomeApp());
 
@@ -25,6 +27,50 @@ class FeedApp extends StatefulWidget {
 class FeedAppState extends State<FeedApp> {
   var _isLoading = true;
 
+  Future<String> _getJsonBody(String url) async  {
+    var client = new http.Client();
+    var response = await client.get(
+          Uri.encodeFull(url),
+          headers: {
+            "Accept": "application/json",
+            "Connection": "keep-alive",
+            "X-Source-App": "Fly-On-Feeds",
+          }
+        );
+    await client.close();
+    if (response.statusCode == 200) {
+      print(convert.jsonDecode(response.body).length);
+      return response.body;
+    }
+    print("request failed with status: ${response.statusCode}.");
+    return '{}';
+  }
+
+  _fetchStory(String storyId) async {
+     print(storyId);
+     final storyBaseUrl = 'https://hacker-news.firebaseio.com/v0/item/';
+     final storyUrl = storyBaseUrl + storyId + '.json';
+
+     String storyJsonBody = await _getJsonBody(storyUrl);
+     print(storyJsonBody.toString());
+     return convert.jsonDecode(storyJsonBody.toString());
+  }
+
+  _fetchFeeds() async {
+     setState(() { _isLoading = true; });
+
+     final topStoriesUrl = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+
+     var topStoriesIdJson = await _getJsonBody(topStoriesUrl);
+      print(convert.jsonDecode(topStoriesIdJson).length);
+     final List<dynamic> topStoriesId = convert.jsonDecode(topStoriesIdJson.toString());
+     var topStories = topStoriesId.sublist(0,2).map(
+         (storyId) => _fetchStory(storyId.toString())
+     ).toList();
+
+     setState(() { _isLoading = false; });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,9 +81,7 @@ class FeedAppState extends State<FeedApp> {
               icon: new Icon(Icons.refresh),
               onPressed: () {
                 print('reloading...');
-                setState(() {
-                  _isLoading = _isLoading ? false : true;
-                });
+                _fetchFeeds();
               },
           )
         ],
